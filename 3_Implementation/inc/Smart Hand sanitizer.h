1a939e7a4c98c8a47c9d1296729194b035db2264
AravinthArumugam <102281509+AravinthArumugam@users.noreply.github.com>
2022-04-23 20:27:44 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Smart Hand sanitizer.h
@@ -0,0 +1,21 @@
+
+
+/**
+ * @file Hand Sanitizer.h
+ * @author ARAVINTH A  (aravinth.18ece@sonatech.ac.in)
+ * @brief SMART HAND SANITIZER 
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <Servo.h>  // include the library for Servo Motor
+
+const int trigPin = 7; 
+
+const int echoPin = 8; 
+
+Servo Myservo;  
+
+
